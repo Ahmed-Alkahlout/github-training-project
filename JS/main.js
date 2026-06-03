99515288297ecaebdccc4cd17a7ba39538369e34
Ahmed Alkahlout <aalkahlout15@smail.ucas.edu.ps>
2026-06-03 23:29:05 +0300

    Make The Website Rosponsive

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,10 @@ window.onscroll = function(){
         header.classList.remove("active")
     }
 }
+
+
+let nav_links = document.getElementById("links");
+
+function open_close_menu(){
+    nav_links.classList.toggle("active")
+}
